5b1f580f0b6553b161cbb0e99af3c6cc48a6debd
timurtm72 <timur.sultanov1972@gmail.com>
2023-03-03 17:13:30 +0300

    Готово

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import models.Person;
+import service.PeopleService;
+
+public class Main {
+    public static void main(String[] args) {
+        PeopleService service = new PeopleService(new Person());
+        service.InitPersons();
+        service.createPeople();
+        service.walletCalc();
+        service.printPeople();
+        service.sortPeople(3);
+    }
 }
--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,2 +1,89 @@
-package models;public class Person {
+package models;
+
+import java.text.DecimalFormat;
+
+public class Person {
+    private String name;
+    private double wallet;
+    private double appendFromBank;
+
+    private double walletAccount;
+
+    public Person() {
+    }
+
+    public Person(String name, double wallet, double appendFromBank) {
+        this.name = name;
+        this.wallet = wallet;
+        this.appendFromBank = appendFromBank;
+    }
+
+    public double getWalletAccount() {
+        return walletAccount;
+    }
+
+    public void setWalletAccount(double walletAccount) {
+        this.walletAccount = walletAccount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(double wallet) {
+        this.wallet = wallet;
+    }
+
+    public double getAppendFromBank() {
+        return appendFromBank;
+    }
+
+    public void setAppendFromBank(double appendFromBank) {
+        this.appendFromBank = appendFromBank;
+    }
+
+    @Override
+    public String toString() {
+        DecimalFormat formatter = new DecimalFormat("#0.00");
+        return "Person " +
+                "name = '" + name + '\'' +
+                ", wallet = " + wallet +
+                ", appendFromBank = " + formatter.format(appendFromBank);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (Double.compare(person.wallet, wallet) != 0) return false;
+        if (Double.compare(person.appendFromBank, appendFromBank) != 0) return false;
+        if (Double.compare(person.walletAccount, walletAccount) != 0) return false;
+        return name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name.hashCode();
+        temp = Double.doubleToLongBits(wallet);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(appendFromBank);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(walletAccount);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
 }
